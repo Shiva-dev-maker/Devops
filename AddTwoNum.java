67c069d2cf93f4e450c6dff35d1f718fd5a13404
Shiva-dev-maker <ucejcse503@gmail.com>
2024-12-11 16:36:25 +0530

    Update AddTwoNum.java

--- AddTwoNum.java
+++ AddTwoNum.java
@@ -8,7 +8,7 @@ public class AndTwoNum {
         }
 
     int num1 = Integer.parseInt(args[0]);
-        int num2 = Integer.parseInt(args[1});
+        int num2 = Integer.parseInt(args[1]);
 
         int sum =num1+ num2;
         System.out.println("The sum of" + num1 + " and" + num2 +"is" + sum);
