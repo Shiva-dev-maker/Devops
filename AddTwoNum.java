b5d8067b3fe4e0fcd67bb587e417f41e1c5644f4
Shiva-dev-maker <ucejcse503@gmail.com>
2024-12-11 16:05:29 +0530

    Add files via upload

---
+++ AddTwoNum.java
@@ -0,0 +1,20 @@
+public class AndTwoNum {
+    public static void main(String args[]){
+        if(args.length <2) {
+    
+
+    System.out.println("Please provide two numbers as arguments");
+    return;
+        }
+
+    int num1 = Integer.parseInt(args[0]);
+        int num1 = Integer.parseInt(args[0]);
+
+        int sum =num1+ num2;
+        System.out.println("The sum of" + num1 + " and" + num2 +"is" + sum);
+    }
+    
+
+
+    
+}
